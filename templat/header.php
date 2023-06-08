0cf3da3b142bfb511e9db78f1e098246916427c0
Arthur Duarte <arthurduartealves99@gmail.com>
2023-06-07 22:01:12 -0300

    Finalizando o sistema de mensagens

--- templat/header.php
+++ templat/header.php
@@ -1,8 +1,16 @@
 <?php
 require_once("global.php");
 require_once("db.php");
+require_once("models/Message.php");
 
-$flashMessage = [];
+
+$message = new Message($BASE_URL);
+
+$flashMessage = $message->getMessage();
+
+if(!empty($flashMessage["msg"])){ 
+  $message ->clearMessage();
+}
 ?>
 
 <!DOCTYPE html>
