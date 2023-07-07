b3825dcf16ff628f9832a12b5def61a4dbee667b
Arthur Duarte <arthurduartealves99@gmail.com>
2023-07-07 02:20:31 -0300

    feat:Realizando incio do backend da adição de filmes

---
+++ movie.php
@@ -0,0 +1,32 @@
+<?php
+    class Movie{
+        public $id;
+        public $titulo;
+        public $imagem;
+        public $categoria;
+        public $tamanho;
+        public $trailer;
+        public $users_id;
+
+        public function generateImageName(){
+            return bin2hex(random_bytes(50)) . ".jpeg";
+        }
+
+    }
+
+    interface MovieDAOInterface{
+        public function buildMovie($data);
+        public function getLatesMovies();
+        public function getMoviesByCategory();
+        public function getMoviesByUserId();
+        public function findAll();
+        public function findById($id);
+        public function findByTitle($titulo);
+        public function create(Movie $movie);
+        public function update(Movie $movie);
+        public function destroy($id);
+
+
+
+    }
+?>
